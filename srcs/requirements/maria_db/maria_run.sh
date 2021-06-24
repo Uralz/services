9ab065a50ac44d544a3ac16c8e24cec523243ba7
wbritni <wbritni@21-school.ru>
2021-06-24 10:49:27 +0500

    mandatory part 2.1

--- srcs/requirements/maria_db/maria_run.sh
+++ srcs/requirements/maria_db/maria_run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 service mysql start
+mariadb -e "use mysql; update user set password=PASSWORD("${ROOT_PWD}") where User='root'; flush privileges; update user set plugin='' where User='root';" &&\
 mariadb -e "CREATE DATABASE IF NOT EXISTS wp DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" &&\
 mariadb -e "CREATE USER '${MARIA_LGN}'@'localhost' IDENTIFIED BY '${MARIA_PWD}';" &&\
 mariadb -e "GRANT ALL PRIVILEGES ON wp.* TO '${MARIA_LGN}'@'%' IDENTIFIED BY '${MARIA_PWD}';" &&\
